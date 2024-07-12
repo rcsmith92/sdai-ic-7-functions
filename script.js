a9556d54a318f4e870459240e463894878f49d7f
Jeni Williams <stanfordjeni@gmail.com>
2024-07-12 05:10:22 +0000

    Completed the activities in the README file

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// Type node script.js in the codespaces console to run this file. Make sure to save before running it. CTRL + ` will open the console if you don't see it.
+// Type node script.js in the codespaces console to run this file. 
+// Make sure to save before running it. CTRL + ` will open the console if you don't see it.
+function isEligibleForFreeShipping(purchaseAmount, isMember) {
+    let freeShipping
+    if (isMember) {
+        freeShipping = true
+    } else if (purchaseAmount >= 50) {
+        freeShipping = true
+    } else {
+        freeShipping = false
+    }
+
+    if (freeShipping) {
+        console.log("Congrats! You are eligible for free shipping!")
+    } else {
+        console.log("Sorry, you are not eligible for free shipping!")
+
+    }
+}
+
+isEligibleForFreeShipping(49, false)
